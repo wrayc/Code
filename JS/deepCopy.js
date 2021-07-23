9fef4bc27ed45afaf086a76374ee91d0a92a55f8
wrayc <rdwrayc@gmail.com>
2021-07-23 13:27:39 +0800

    update

--- JS/deepCopy.js
+++ JS/deepCopy.js
@@ -36,3 +36,51 @@ let obj3 = [[11, 22], [22, 33]]
 console.log(deepCopy(obj1))
 console.log(deepCopy(obj2))
 console.log(deepCopy(obj3))
+
+function deepCopy2(target){
+    if(Object.prototype.toString.call(target) === '[object Object]'){
+        let dist;
+        switch (Object.prototype.toString.call(target).slice(8,-1).toLowerCase()){
+            case 'array':
+                dist = [];
+                break;
+            case 'function':
+                dist = function (){
+                    return target.call(this,...arguments)
+                };
+                break;
+            case 'regExp':
+                dist = new RegExp(target.source,target.flags);
+                break;
+            case 'date':
+                dist = new Date(target);
+                break;
+            default:
+                dist = {};
+                break;
+        }
+        for (let key in target){
+            if (target.hasOwnProperty(key)){
+                dist[key] = deepCopy(target[key])
+            }
+
+        }
+        return dist;
+    }else {
+        return target;
+    }
+}
+
+let obj5 = {
+    name: 'WRay',
+    age: 20,
+    f:function (){
+        console.log('22')
+    },
+    date:Date.now(),
+    re: /\whello/gi
+}
+let obj6 = deepCopy2(obj5)
+console.log(obj6)
+
+
--- JS/test.js
+++ JS/test.js
@@ -1,48 +1,20 @@
-function deepCopy(target){
-    if(Object.prototype.toString.call(target) === '[object Object]'){
-        let dist;
-        switch (Object.prototype.toString.call(target).slice(8,-1).toLowerCase()){
-            case 'array':
-                dist = [];
-                break;
-            case 'function':
-                dist = function (){
-                    return target.call(this,...arguments)
-                };
-                break;
-            case 'regExp':
-                dist = new RegExp(target.source,target.flags);
-                break;
-            case 'date':
-                dist = new Date(target);
-                break;
-            default:
-                dist = {};
-                break;
-        }
-        for (let key in target){
-            if (target.hasOwnProperty(key)){
-                dist[key] = deepCopy(target[key])
-            }
+function _new(func){
+    let obj =Object.create(func.prototype)
 
-        }
-        return dist;
-    }else {
-        return target;
+    let result = func.apply(obj)
+    if(result && (typeof (result) == "object" || typeof (result) == "function")){
+        return result
     }
+    return obj
 }
 
-let obj1 = {
-    name: 'WRay',
-    age: 20,
-    f:function (){
-        console.log('22')
-    },
-    date:Date.now(),
-    re: /\whello/gi
+// fn.call(thisArg,param1,param2)
+// fn.apply(thisArg,[param1,param2])
+// fn.bind(thisArg,param1,param2)
+function fn(x1,x2){
+    console.log(this,x1,x2)
 }
-let obj2 = deepCopy(obj1)
-console.log(obj2)
-console.log(obj2 === obj1)
-
-
+let o1 = {x:200}
+fn.call(20,20,30)
+fn.apply('str',[50,60])
+fn.bind(true,70,80)()
